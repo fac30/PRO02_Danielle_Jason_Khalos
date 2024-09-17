c1ff166e838273e0a68ec3abde11cb83e3d73bba
Jason Warren <contact.jwarren@gmail.com>
2024-09-17 17:11:49 +0100

    added code to command handler

--- discord/app.js
+++ discord/app.js
@@ -66,7 +66,24 @@ for (const sub of comSubs) { /* return Collection of all files within /discord/c
 console.log(client.commands);
 
 //h2 Command Handler
-client.on(Events.InteractionCreate, interaction => {
+client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
-	console.log(interaction);
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
 });
