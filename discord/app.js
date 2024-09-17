c06b03a832b7bce699fd8c3dd0f1e2338b69a05c
Jason Warren <contact.jwarren@gmail.com>
2024-09-17 14:31:13 +0100

    feat(discord): log into Discord as bot
    - imports keys from .`.env` & stores in object
    - imports classes from `discord.js`
    - creates new client
    - bot logs in to client
    - console logs success message

--- discord/app.js
+++ discord/app.js
@@ -1,6 +1,30 @@
-const keys = require('dotenv');
-keys.config();
+// h1 IMPORTS & DECLARATIONS
 
-const appId = process.env.DISCORD_APP_ID;
-const pubKey = process.env.DISCORD_PUBLIC_KEY;
-const token = process.env.DISCORD_TOKEN;
+// n1 Secrets
+const dotenv = require('dotenv').config();
+const keys = {
+    discord: {
+        id: process.env.DISCORD_APP_ID,
+        public: process.env.DISCORD_PUBLIC_KEY,
+        token: process.env.DISCORD_TOKEN,
+    },
+    openai: { token: process.env.OPENAI_TOKEN, }
+};
+
+// n1 Libraries
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+// h1 SETUP
+
+// n1 Create Client
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds]
+});
+
+// n1 Confirm client is ready in terminal
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// n1 Login using discord token
+client.login(keys.discord.token);
