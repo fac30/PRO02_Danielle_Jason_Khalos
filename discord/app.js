60da9649ff9af5405aec8f776dcba35ab684ce13
Jason Warren <contact.jwarren@gmail.com>
2024-09-17 13:09:07 +0100

    build (discord): define key variables from `.env`

--- discord/app.js
+++ discord/app.js
@@ -1,9 +1,6 @@
 const keys = require('dotenv');
-
 keys.config();
 
-console.groupCollapsed("Is your .env file set up correctly?");
-console.log(process.env.DISCORD_APP_ID);
-console.log(process.env.DISCORD_PUBLIC_KEY);
-console.log(process.env.DISCORD_TOKEN);
-console.groupEnd();
+const appId = process.env.DISCORD_APP_ID;
+const pubKey = process.env.DISCORD_PUBLIC_KEY;
+const token = process.env.DISCORD_TOKEN;
