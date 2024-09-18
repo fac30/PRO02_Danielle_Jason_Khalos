2813c5adc4405e224f56a283f23820f8397fc69f
Jason Warren <contact.jwarren@gmail.com>
2024-09-18 12:18:30 +0100

    feat(bot commands): deploy commands script
    last commit before test deployment

--- discord/deploy-commands.js
+++ discord/deploy-commands.js
@@ -10,48 +10,49 @@ const keys = {
     },
     openai: { token: process.env.OPENAI_TOKEN, }
 };
+
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
+const comList = require("./list-commands");
 
-const commands = [];
-// Grab all the command folders from the commands directory you created earlier
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+function buildComArr() {
+	const commands = [];
 
-for (const folder of commandFolders) {
-	// Grab all the command files from the commands directory you created earlier
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			commands.push(command.data.toJSON());
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	const comPath = path.join(__dirname, 'commands');
+	const comSubs = fs.readdirSync(comPath);
+
+	for (const sub of comSubs) {
+		const subPath = path.join(comPath, sub);
+		const subFiles = fs.readdirSync(subPath)
+			.filter(file => file.endsWith('.js'));
+		for (const file of subFiles) {
+			const filePath = path.join(subPath, file);
+			const com = require(filePath);
+			if ('data' in com && 'execute' in com) {
+				commands.push(com.data.toJSON());
+			} else {
+				console.log(`${filePath} lacks "data"/"execute" property.`);
+			}
 		}
 	}
+
+	return commands;
 }
 
-// Construct and prepare an instance of the REST module
+const commands = buildComArr();
+
 const rest = new REST().setToken(token);
 
-// and deploy your commands!
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
-		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
 		);
-
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
 		console.error(error);
 	}
 })();
