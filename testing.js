b729e7640289a89885bc86f2741bbf2b508ab0f5
Khalos Moscato <khalos.moscato@icloud.com>
2024-09-18 16:31:26 +0100

    minor changes to testing file'

--- testing.js
+++ testing.js
@@ -5,12 +5,14 @@ const serverBridge = require('./app');
 module.exports = {
   // Simulates Discord messages and the response function
   listenToMessages: async (handleMessage) => {
-    const testMessage = "What is the capital of Japan?"; // Example test message
+    const testMessage = "What is the capital of France?"; // Example test message
     
     // Simulate a reply function
+    
     const reply = (response) => {
-      console.log("Replying with:", response);
-    };
+      // console.log(typeof reply());
+      console.log('Replying with:', response);
+    }
 
     // Call the handler function with the test message and reply function
     await handleMessage(testMessage, reply);
