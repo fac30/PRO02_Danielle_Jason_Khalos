5e09cdc62746827b0019e054604b9722b821078a
Jason Warren <contact.jwarren@gmail.com>
2024-09-18 15:07:37 +0100

    build(discord & server): imports & setup
    **`./app.js`**
    - renamed `./discord/app.js` to `./discord/discord.js`
    - changed import: `const tests = "./testing`
    - added imports: `const discord = "./discord/discord`
    - bot now logs into discord from this file

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const dotenv = require('dotenv').config();
 
 const openAiKey = process.env.OPENAI_TOKEN;
-console.log('OpenAI Token:', OPENAI_TOKEN);
+const keys = { //discord keys
+    discord: {
+        id: process.env.DISCORD_APP_ID,
+        public: process.env.DISCORD_PUBLIC_KEY,
+        server: process.env.DISCORD_SERVER,
+        token: process.env.DISCORD_TOKEN,
+    }
+};
+console.log('OpenAI Token:', openAiKey);
 
-const discord = require('./testing');
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const discord = require('./discord/discord');
 const openai = require('./openai/app');
+const tests = require('./testing');
 
-// Listen for Discord messages
-discord.listenToMessages(async (message, reply) => {
+//h1 Starts Bot - DO NOT TOUCH
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.once(Events.ClientReady, readyClient => {
+    console.log(`${readyClient.user.tag} ready`);
+});
+client.login(keys.discord.token);
+
+//h1 Listen for Discord messages
+tests.listenToMessages(async (message, reply) => {
   try {
   // Pass the message to OpenAI and get a response
   const openAIResponse = await openai.chatPrompt(message);
