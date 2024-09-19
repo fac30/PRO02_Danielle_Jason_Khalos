04bc3482d052e74d97e60cb6b39557bf28928683
Jason Warren <contact.jwarren@gmail.com>
2024-09-19 11:06:30 +0100

    nearly there

--- discord/deploy-commands.js
+++ discord/deploy-commands.js
@@ -1,4 +1,5 @@
 //h1 IMPORTS & DECLARATIONS
+console.group(`========= deploy-commands.js: imports ======`);
 const dotenv = require('dotenv');
 dotenv.config();
 const keys = {
@@ -14,44 +15,62 @@ const keys = {
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
+console.log(`deploy-commands.js: imports done`);
+console.groupEnd();
 
 function buildComArr() {
+	console.group(`============ deploy-commands.js: buildComArr ============`);
 	const commands = [];
 
 	const comPath = path.join(__dirname, 'commands');
 	const comSubs = fs.readdirSync(comPath);
 
 	for (const sub of comSubs) {
+		console.group(`============ deploy-commands.js: ${sub} subfolder ============`);
 		const subPath = path.join(comPath, sub);
 		const subFiles = fs.readdirSync(subPath)
 			.filter(file => file.endsWith('.js'));
 		for (const file of subFiles) {
+			console.group(`============== deploy-commands.js: ${file} command ==============`);
 			const filePath = path.join(subPath, file);
+			console.log(`deploy-commands.js: found? ${filePath != null}`);
+			console.log(`IF IT LOGS FROM ANOTHER FILE AFTER THIS, THE PROBLEM IS HERE the fucking prick`)
 			const com = require(filePath);
+			console.log(`deploy-commands.js: ${com}`);
 			if ('data' in com && 'execute' in com) {
 				commands.push(com.data.toJSON());
+				console.log(`deploy-commands.js: Pushed`)
 			} else {
-				console.log(`${filePath} lacks "data"/"execute" property.`);
+				console.log(`deploy-commands.js: ${file} lacks "data"/"execute" property.`);
 			}
+			console.groupEnd();
 		}
+		console.groupEnd();
 	}
-
+	console.groupEnd();
 	return commands;
 }
 
-const commands = buildComArr();
-
+/* const commands = buildComArr(); */
+console.group(`========= deploy-commands.js: create REST =========`);
+console.log(`deploy-commands.js: about to create new REST`);
 const rest = new REST().setToken(keys.discord.token);
+console.log(`deploy-commands.js: REST created`);
+console.groupEnd();
 
 (async () => {
+	console.group(`============ deploy-commands.js: async function ============`);
 	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		console.group(`============ deploy-commands.js: try branch ============`);
 		const data = await rest.put(
 			Routes.applicationGuildCommands(keys.discord.id, keys.discord.server),
-			{ body: commands },
+			{ body: buildComArr() },
 		);
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		console.log(`deploy-commands.js: Successfully reloaded ${data.length} application (/) commands.`);
+		console.groupEnd();
 	} catch (error) {
+		console.group(`============ deploy-commands.js: catch branch ============`);
 		console.error(error);
+		console.groupEnd();
 	}
 })();
